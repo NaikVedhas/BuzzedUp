9420d53d88495979cbcedcda42477fde1aee2d1e
NaikVedhas <vedhasnaik121@gmail.com>
2024-10-23 17:53:57 +0530

    Fixed Bug

--- Project/src/components/Blogs.jsx
+++ Project/src/components/Blogs.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import { Link } from "react-router-dom";
 import BlogCard from "./BlogCard";
 import useFetch from "./useFetch";
